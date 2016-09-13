b9025280ece8792229b41cc127995b9d19109a14
Maldon <maldonmeehan@gmail.com>
2016-09-13 15:31:47 -0700

    add update and delete functions for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,6 +14,26 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    if @answer.update(answer_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to questions_path
+  end
+
   private
     def answer_params
       params.require(:answer).permit(:user, :content)
