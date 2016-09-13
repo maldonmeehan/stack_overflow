42fead6caabe04a6bd1ce3def5098c215872dcc0
Maldon <maldonmeehan@gmail.com>
2016-09-13 15:05:54 -0700

    add delete function for question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,6 +33,12 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+  
   private
   def question_params
     params.require(:question).permit(:user, :content)
