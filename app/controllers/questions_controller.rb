d9c402f3c0c49cfd630960641d26359da8452238
Maldon <maldonmeehan@gmail.com>
2016-09-13 14:58:39 -0700

    add create function for question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,22 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
-  
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:user, :content)
+  end
 end
